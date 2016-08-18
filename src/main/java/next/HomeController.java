e28569c4cfbcd6d8547ad7ec89df2b8ac19beedd
t1t1 <cha@hanmail2.net>
2016-08-18 21:29:24 +0900

    tomcat-embed-jasper, jstl 설정

--- src/main/java/next/HomeController.java
+++ src/main/java/next/HomeController.java
@@ -1,14 +1,16 @@
 package next;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
+@Controller
 public class HomeController {
 	
 	@RequestMapping("/")
 	public String home() {
-		return "Hello World";
+		return "home";
 	}
 
 }
